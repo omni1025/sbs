129e99f897b5c0f8c14849da98cd7379bc6bdca8
omnibiomed <omnibiomedsolutions@gmail.com>
2026-03-19 15:24:46 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+// LOADER
+window.onload = () => {
+  document.getElementById("loader").style.display = "none";
+};
 
+// DARK MODE
+document.getElementById("darkToggle").onclick = () => {
+  document.body.classList.toggle("dark");
+};
+
+// FILTER
+function filter(type){
+  let cards = document.querySelectorAll(".bed-card");
+
+  cards.forEach(card => {
+    if(type === "all" || card.classList.contains(type)){
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+}
